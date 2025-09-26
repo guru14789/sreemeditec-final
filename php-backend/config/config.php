e0a42826c2f82258b21ca7fdfd80b278704e189e
maviiispams17 <maviiispams17@gmail.com>
2025-09-26 13:40:22 +0000

    update

--- php-backend/config/config.php
+++ php-backend/config/config.php
@@ -12,8 +12,8 @@ if ($_ENV['APP_DEBUG'] === 'true') {
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
 } else {
-    error_reporting(0);
-    ini_set('display_errors', 0);
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
 }
 
 // Set timezone
