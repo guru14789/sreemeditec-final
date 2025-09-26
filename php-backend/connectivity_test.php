e3f91d7c4b2a2d9e74433da18754be41a1dd6188
maviiispams17 <maviiispams17@gmail.com>
2025-09-26 06:36:39 +0000

    first commit

--- php-backend/connectivity_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/config/db.php';
-
-if (DatabaseConnection::testConnection()) {
-    echo "Database connection successful.\n";
-} else {
-    echo "Database connection failed.\n";
-}
-?>
